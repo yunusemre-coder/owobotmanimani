fc311187fcdd22ca12783880d333f69343414c6c
yunusemre-coder <60299186+yunusemre-coder@users.noreply.github.com>
2021-06-13 14:27:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+const {
+	JsonDatabase
+} = require("wio.db");
+const db = new JsonDatabase("db");
+var kanal = "830873053184917506";
+var sahip = "842722569907208192";
+
+client.on("ready",()=>{
+	console.log("Hazır ! ");
+});
+
+//ArdaDemr Youtube
+
+setInterval(function(){
+	if(db.get("owodg") != "1"){
+		client.channels.get(kanal).send("owo h");
+	};
+},15500);
+//ArdaDemr Youtube
+client.on("message",message=>{
+	if(message.channel.id==kanal || message.channel.type == "dm"){
+		if(message.author.id=="408785106942164992"){
+			if(message.content.includes("Beep Boop") || message.content.includes("Please DM me") || message.content.includes("human") || message.content.includes("check")){
+				db.set("owodg","1");
+				message.channel.send("Durduruldu açmak için !aç");
+			};
+		};
+	};
+
+	if(message.author.id == sahip){
+		if(message.content == "!aç"){
+			db.set("owodg","0");
+			message.channel.send("Açıldı");
+		};
+	};
+});
+
+//ArdaDemr Youtube
+
+client.login("token");
